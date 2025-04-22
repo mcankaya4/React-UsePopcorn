85c865ec5faafdba93a88004ed7b1e1e55227892
mcankaya4 <mcankaya4@gmail.com>
2025-04-22 21:49:10 +0300

    update map

--- src/component/WatchedSummary.js
+++ src/component/WatchedSummary.js
@@ -1,7 +1,7 @@
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-function WatchedSummary({ watched }) {
+function WatchedSummary({ watched = [] }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
